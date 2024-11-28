9cdfd75c269e0c9a344584fe9ad09248df868e8d
Sai vamshi Gannoju <saivamshig404@gmail.com>
2024-11-29 00:16:30 +0530

    Add vercel.json for configuration

--- flash-designs/next.config.ts
+++ flash-designs/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    domains: ["https://www.freepik.com"],
-  },
 };
 
 export default nextConfig;
